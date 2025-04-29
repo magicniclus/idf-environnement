f3de6feb4cc6400f368be68125f11dcd5d790868
Nicolas <72094122+magicniclus@users.noreply.github.com>
2025-04-29 15:18:29 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Fonctionnement from "@/components/sections/fonctionnement/Fonctionnement";
 import Hero from "@/components/sections/heros/Hero";
 
 export default function Home() {
   return (
     <div className="relative">
       <Hero />
+      <Fonctionnement />
     </div>
   );
 }
