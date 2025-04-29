017a58371c3e262fdb0ad833fcf57473b2a5d7e4
Nicolas <72094122+magicniclus@users.noreply.github.com>
2025-04-29 21:34:54 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
+import Certifications from "@/components/sections/certifications/Certifications";
+import Faq from "@/components/sections/faq/Faq";
+import Footer from "@/components/sections/footer/Footer";
 import Fonctionnement from "@/components/sections/fonctionnement/Fonctionnement";
 import Hero from "@/components/sections/heros/Hero";
+import Prestation from "@/components/sections/prestation/Prestation";
 
 export default function Home() {
   return (
     <div className="relative">
       <Hero />
+      <Certifications />
       <Fonctionnement />
+      <Prestation />
+      <Faq />
+      <Footer />
     </div>
   );
 }
